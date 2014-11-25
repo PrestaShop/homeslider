8237effc4403c706c2f44fcf634c8bea0fcf2a5d
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-11-25 09:41:50 +0100

    // Version updated to v1.4.3

--- homeslider.php
+++ homeslider.php
@@ -45,7 +45,7 @@ class HomeSlider extends Module
 	{
 		$this->name = 'homeslider';
 		$this->tab = 'front_office_features';
-		$this->version = '1.4.2';
+		$this->version = '1.4.3';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 		$this->secure_key = Tools::encrypt($this->name);
